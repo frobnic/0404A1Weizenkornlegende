7bd688fb3e57b89db9c888352cc93f9181fc871c
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 11:30:59 +0200

    code inserted

--- 0404A1Weizenkornlegende/Program.cs
+++ 0404A1Weizenkornlegende/Program.cs
@@ -10,6 +10,27 @@ namespace _0404A1Weizenkornlegende
     {
         static void Main(string[] args)
         {
+            double w=1;
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Auf dem {0,2}. Feld liegen {1} Körner",i+1, w);
+                w = w * 2;
+                i++;
+            } while (i < 64);
+
+            Console.WriteLine();
+
+            double tonnen =  w / 1000 / 1000 / 0.5;
+            Console.WriteLine("Das macht etwa {0} Tonnen Getreide.", tonnen);
+
+            Console.WriteLine();
+            double stau = tonnen / 25 * 16 / 1000;
+            Console.WriteLine("Der Stau wäre etwa {0:F2} km lang.", stau);
+
+            Console.WriteLine();
+
         }
     }
 }
